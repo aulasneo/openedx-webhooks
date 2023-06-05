b5f5a3b82f0e26c54a992de90ef4d21bcfaad3ce
Andrés González <andres@aulasneo.com>
2023-06-05 16:33:27 -0300

    Add about file

---
+++ webhooks/__about__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+"""
+About file.
+
+written by:     Andrés González
+                https://aulasneo.com
+
+date:           May-2023
+
+usage:          semantic version control for openedx-webhooks
+"""
+__version__ = "0.1.0"
